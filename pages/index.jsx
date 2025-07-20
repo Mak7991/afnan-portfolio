47433cf991f4e4e66a6049a6e4ff0c12a87d63a0
Mak7991 <alikhanmaaz1997@gmail.com>
2025-07-20 15:49:19 +0500

    minor changes in css and home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,10 +8,10 @@ import { fadeIn } from "../variants";
 
 const Home = () => {
   return (
-    <div className="bg-primary/60 h-full overflow-auto">
+    <div className="bg-primary/60 h-full home-container">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto home-text">
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
